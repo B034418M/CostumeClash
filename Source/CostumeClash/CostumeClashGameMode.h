1686a81c4a4288cd8ded6d4b32fc58bfc664e63a
d016527j <d016527j@C108-40>
2024-03-18 16:44:38 +0000

    Init UE5 upload

---
+++ Source/CostumeClash/CostumeClashGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CostumeClashGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ACostumeClashGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ACostumeClashGameMode();
+};
+
+
+
